20da286e8a49059e4091b0f437a96fa8fed46cff
tuanpmdev156 <tuanpmdev156@gmail.com>
2026-05-13 17:04:03 +0700

    Complete grains

---
+++ grains/grains.py
@@ -0,0 +1,10 @@
+def square(number):
+    if number not in range(1,65):
+        raise ValueError("square must be between 1 and 64")
+    return 2**(number-1)
+
+def total(): 
+    sum = 0
+    for number in range (1,65):
+        sum += 2**(number-1)
+    return sum
